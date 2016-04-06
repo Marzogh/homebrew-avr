4a072645c92708e59aaf8ffea82fbdb4b5f76bda
Prajwal Bhattaram <Marzogh@users.noreply.github.com>
2016-04-06 14:53:42 +1000

    Updated SHA256

--- avr-gcc.rb
+++ avr-gcc.rb
@@ -8,7 +8,7 @@ class AvrGcc < Formula
     homepage 'http://www.gnu.org/software/gcc/gcc.html'
     url 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-5.3.0/gcc-5.3.0.tar.bz2'
     mirror 'http://ftpmirror.gnu.org/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2'
-    sha256 'd619847383405fd389f5a2d7225f97fedb01f81478dbb8339047ccba7561a5c20045ab500a8744bffd19ea51892ce09fc37f862f2cfcb42de0f0f8cd8f8da37a'
+    sha256 'b84f5592e9218b73dbae612b5253035a7b34a9a1f7688d2e1bfaaf7267d5c4db'
 
     depends_on 'gmp'
     depends_on 'libmpc'
